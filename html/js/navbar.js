be6aa237d6d3bbc984d107cc2e4886bbb43b0a7b
Timothy Rascher <timrascher@gmail.com>
2024-01-26 12:04:56 -0800

    Finished Configuring the Site

--- html/js/navbar.js
+++ html/js/navbar.js
@@ -1,18 +1,57 @@
+/**
+ * @callback EventHandler
+ * @param {JQuery<HTMLElement>} selectedNavItem
+ */
+
+function DI(item) { return item.attr("data-id") }
+
 class NavController {
     /** @type {JQuery<HTMLElement>} */
     navlinks
     /** @type {JQuery<HTMLElement>} */
     active
+    /**
+     * @private
+     * @type {object}
+     */
+    eventStore = {}
+    events = Object.freeze({
+        navChanged: "navChanged"
+    })
 
     constructor() {
         this.navlinks = $(".navbar-nav .nav-link")
-        this.target = this.navlinks.find(e => $(e).hasClass("active"))
+        this.active = $("a.nav-link.active").first()
         this.navlinks.on("click", (e) => {
             e.preventDefault()
-            this.navlinks.removeClass("active")
             let target = $(e.target)
+            if (DI(this.active) == DI(target)) { return }
+            this.navlinks.removeClass("active")
             target.addClass("active")
             this.active = target
+            this.emit(this.events.navChanged, target)
+        })
+    }
+    /**
+     * @param {string} event 
+     * @param {EventHandler} handler 
+     */
+    on(event, handler) {
+        if (!this.eventStore[event]) { this.eventStore[event] = [] }
+        this.eventStore[event].push(handler)
+        switch (event) {
+            case this.events.navChanged: handler(this.active)
+        }
+    }
+    /**
+     * @private
+     * @param {string} event 
+     * @param {JQuery<HTMLElement>} value
+     */
+    emit(event, value) {
+        if (!this.eventStore[event]) { return }
+        this.eventStore[event].forEach(handler => {
+            handler(value)
         })
     }
 }
--- html/js/site.js
+++ html/js/site.js
@@ -1,4 +1,7 @@
 import navbar from "./navbar.js"
 
 $(async () => {
+    navbar.on(navbar.events.navChanged, (ele) => {
+        console.log(ele.attr("data-id"))
+    })
 })
